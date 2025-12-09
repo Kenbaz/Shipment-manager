bd8dbdce0521ee2f77363cf0bf006d8da7e5400c
Kenbaz <kbassey016@gmail.com>
2025-12-09 16:19:13 +0100

    middleware index file for exports

---
+++ src/middlewares/index.ts
@@ -0,0 +1,13 @@
+export { asyncHandler } from "./asyncHandler";
+export {
+  validateRequest,
+  validateBody,
+  validateParams,
+  validateQuery,
+  type ValidationSource,
+} from "./validateRequest";
+export {
+  errorHandler,
+  notFoundHandler,
+  setupErrorListeners,
+} from "./errorHanlder";
